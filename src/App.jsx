ce3502275f3aae41eba7f9fff9990e0e5ba8276a
De Zaldua Samuel <dezalduasamuel@MBP-de-De.lan>
2020-10-13 11:24:44 +0200

    renamed App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         avatar="https://randomuser.me/api/portraits/women/66.jpg"
         online
       />
+      <ContactList />
     </div>
   );
 }
